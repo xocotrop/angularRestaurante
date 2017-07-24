175cf7b9a7e45d434b95c4fcf4e4007d92a0d7c2
Igor Janoski dos Santos <igor.santos@ciashop.local>
2017-07-24 14:35:44 -0300

    reaproveitamento + componente order item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,9 @@ import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shoppin
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { OrderComponent } from './order/order.component';
+import { InputComponent } from './shared/input/input.component';
+import { RadioComponent } from './shared/radio/radio.component';
+import { OrderItemsComponent } from './order/order-items/order-items.component';
 
 
 @NgModule({
@@ -35,7 +38,10 @@ import { OrderComponent } from './order/order.component';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent
+    OrderComponent,
+    InputComponent,
+    RadioComponent,
+    OrderItemsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/radio/radio.component.ts
@@ -0,0 +1,47 @@
+import { RadioOption } from './radio-option.model';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
+
+@Component({
+  selector: 'mt-radio',
+  templateUrl: './radio.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+  ]
+})
+export class RadioComponent implements OnInit, ControlValueAccessor {
+  
+  onChange: any;
+
+  @Input() options: RadioOption[]
+  value: any
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  setValue(value: any){
+    this.value = value
+    this.onChange(this.value)
+  }
+
+  writeValue(obj: any): void {
+    this.value = obj
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn
+    
+  }
+  registerOnTouched(fn: any): void {
+    
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    
+  }
+
+}
