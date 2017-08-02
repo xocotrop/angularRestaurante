2e415ab263360405d3afbdff2fba2273a097c355
Igor Janoski dos Santos <igor.santos@ciashop.local>
2017-08-02 13:39:17 -0300

    configuração de ambiente + ajuste de navegação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
 
 
 @NgModule({
@@ -45,6 +46,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
   providers: [
+    {provide: LocationStrategy, useClass: HashLocationStrategy},
      {provide: LOCALE_ID, useValue: 'pt-BR'}
   ],
   bootstrap: [AppComponent]
